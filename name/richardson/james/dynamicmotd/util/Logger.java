ad12f19ec3545f29c0cdfdf745083cc77ed7c431
James Richardson <me+github@james.richardson.name>
2011-12-11 17:05:59 +0000

    updated logger

--- name/richardson/james/dynamicmotd/util/Logger.java
+++ name/richardson/james/dynamicmotd/util/Logger.java
@@ -1,52 +1,139 @@
 /*******************************************************************************
  * Copyright (c) 2011 James Richardson.
  * 
- * Logger.java is part of DynamicMOTD.
+ * PluginLogger.java is part of Reservation.
  * 
- * DynamicMOTD is free software: you can redistribute it and/or modify it 
- * under the terms of the GNU General Public License as published by the Free 
- * Software Foundation, either version 3 of the License, or (at your option) 
- * any later version.
+ * Reservation is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
  * 
- * DynamicMOTD is distributed in the hope that it will be useful, but WITHOUT ANY 
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
- * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ * Reservation is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
  * 
- * You should have received a copy of the GNU General Public License 
- * along with DynamicMOTD.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License along with
+ * Reservation. If not, see <http://www.gnu.org/licenses/>.
  ******************************************************************************/
+
 package name.richardson.james.dynamicmotd.util;
 
+import java.util.HashSet;
+import java.util.Set;
+import java.util.logging.Handler;
 import java.util.logging.Level;
 
 public class Logger {
-  
-  static private java.util.logging.Logger logger = java.util.logging.Logger.getLogger("Minecraft");
-  static private String prefix = "[DynamicMOTD] ";
-  static private Level debugLevel = Level.FINE;
-  
-  public static void info(String message) {
-    logger.info(prefix + message);
+
+  private static boolean currentlyDebugging = false;
+  private static final Level debugLevel = Level.ALL;
+  private static final java.util.logging.Logger parentLogger = java.util.logging.Logger.getLogger("Minecraft");
+  private static final String prefix = "[Reservation] ";
+  private static final Set<Logger> registeredLoggers = new HashSet<Logger>();
+
+  private final java.util.logging.Logger logger;
+
+  /**
+   * Create a new logger with the specified name.
+   * 
+   * @param className
+   * The name of the logger, should be the class it belongs to.
+   */
+  public Logger(final Class<?> parentClass) {
+    this.logger = java.util.logging.Logger.getLogger(parentClass.getName());
+    this.logger.setParent(Logger.parentLogger);
+    Logger.registeredLoggers.add(this);
+    if (Logger.currentlyDebugging) {
+      this.setDebugging(true);
+    }
   }
-  
-  public static void warning(String message) {
-    logger.warning(prefix + message);
+
+  /**
+   * Enable debugging for all loggers.
+   * 
+   * This basically sets all parentHandlers to a lower log level to ensure
+   * that messages are correctly logged. All newly created and existing loggers
+   * will also have debugging enabled.
+   */
+  public static void enableDebugging() {
+    Logger.currentlyDebugging = true;
+    for (final Handler handler : Logger.parentLogger.getHandlers()) {
+      handler.setLevel(Logger.debugLevel);
+    }
+    for (final Logger logger : Logger.registeredLoggers) {
+      logger.setDebugging(true);
+    }
+    parentLogger.fine(prefix + "Debugging is now enabled.");
   }
-  
-  public static void severe(String message) {
-    logger.severe(prefix + message);
+
+  /**
+   * Log a configuration message with this logger.
+   * 
+   * @param message
+   * The string that you wish to log.
+   */
+  public void config(final String message) {
+    this.logger.config(Logger.prefix + "<" + this.logger.getName() + "> " + message);
   }
-  
-  public static void debug(String message) {
-    logger.fine(prefix + message);
+
+  /**
+   * Log a debug message with this logger.
+   * 
+   * @param message
+   * The string that you wish to log.
+   */
+  public void debug(final String message) {
+    this.logger.fine(Logger.prefix + "<" + this.logger.getName() + "> " + message);
   }
-  
-  public static boolean isDebugging() {
-    return logger.isLoggable(debugLevel);
+
+  /**
+   * Log a general message with this logger.
+   * 
+   * @param message
+   * The string that you wish to log.
+   */
+  public void info(final String message) {
+    this.logger.info(Logger.prefix + message);
   }
-  
-  public static void setDebugging(Boolean value) {
-    logger.setLevel(debugLevel);
+
+  /**
+   * Check to see if the logger is logging debug messages.
+   * 
+   * @return isDebugging true if it is logging debug messages, false otherwise.
+   */
+  public boolean isDebugging() {
+    return this.logger.isLoggable(Logger.debugLevel);
   }
-  
+
+  /**
+   * Set if a logger should be logging debug messages or not.
+   * 
+   * @param setDebugging
+   * true if it is should log messages, false otherwise.
+   */
+  public void setDebugging(final Boolean value) {
+    this.logger.setLevel(Logger.debugLevel);
+  }
+
+  /**
+   * Log a severe (fatal) message with this logger.
+   * 
+   * @param message
+   * The string that you wish to log.
+   */
+  public void severe(final String message) {
+    this.logger.severe(Logger.prefix + message);
+  }
+
+  /**
+   * Log a warning message with this logger.
+   * 
+   * @param message
+   * The string that you wish to log.
+   */
+  public void warning(final String message) {
+    this.logger.warning(Logger.prefix + message);
+  }
+
 }
